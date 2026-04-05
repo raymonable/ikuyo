46affc22063e9bdabaf591bddbaa369d74cbce34
Raymond <101374892+raymonable@users.noreply.github.com>
2026-04-05 03:37:44 -0400

    fix: [txp] add more information

--- src/format/aft/txp.c
+++ src/format/aft/txp.c
@@ -20,6 +20,7 @@ struct TextureInformation txpReadBuffer(uint8_t* buffer) {
     uint32_t mapCount = bytestreamReadLong(&bytestream, false);
     bytestreamReadLong(&bytestream, false);
 
+    // NOTE: txp can contain multiple textures in a single file, so it has to be more complex than a simple TextureInformation
     for (size_t index = 0; index < mapCount; index++) {
         struct Bytestream textureBytestream = bytestreamInit(buffer);
         uint32_t textureFileOffset = bytestreamReadLong(&bytestream, false);;
@@ -41,6 +42,22 @@ struct TextureInformation txpReadBuffer(uint8_t* buffer) {
             uint32_t width = bytestreamReadLong(&mipBytestream, false);
             uint32_t height = bytestreamReadLong(&mipBytestream, false);
             uint32_t format = bytestreamReadLong(&mipBytestream, false);
+            uint32_t id = bytestreamReadLong(&mipBytestream, false);
+            uint32_t bufferSize = bytestreamReadLong(&mipBytestream, false);
+
+            enum TextureEncoding encoding = UnsupportedEncoding;
+            switch (format) {
+                case 1: encoding = RGB; break;
+                case 2: encoding = RGBA; break;
+                case 6: case 7: encoding = DXT1; break;
+                case 8: encoding = DXT3; break;
+                case 9: encoding = DXT5; break;
+                // NOTE: our target is AFT, this is redundant as this only appears in MM+ , but it's only an extra statement
+                case 15: encoding = BC7; break;
+                default: break;
+            }
+
+            // NOTE: next is the data... to be implemented
         }
     }
 
