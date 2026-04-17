69f28d8d629fe17b6c1a9040346d630ba438b811
raymonable <101374892+raymonable@users.noreply.github.com>
2026-04-17 16:46:22 -0400

    feat: :construction: begin work on jni

---
+++ src/interface/jni.c
@@ -0,0 +1,13 @@
+//
+// Created by Raymond on 4/17/26.
+//
+
+#include <common/texture.h>
+#include <common/image.h>
+
+#include <jni.h>
+#include <stdio.h>
+
+JNIEXPORT void JNICALL Java_main_Ikuyo_init() {
+    printf("hopping dick dick dick\n");
+}
